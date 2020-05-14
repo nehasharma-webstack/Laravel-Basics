af7f2470a19ea9182fc699e902e475d1fda2f1fe
neha-webstack <nehaweb1993@gmail.com>
2020-05-14 12:20:38 +0530

    Creating a Nav Bar

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+    //return "Contact";
+    //return ["name"=>"laptop","price"=>10000];
+});
+
+Route::get('/aboutus',function(){
+    return view('aboutus');
+});
+
+Route::get('/products',function(){
+    return view('products');
+});
+
+Route::get('/signup',function(){
+    return view('signup');
+});
+
+
