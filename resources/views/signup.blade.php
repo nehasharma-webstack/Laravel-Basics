39e566f278c397342b16c2d30019443b1dd25d7a
neha-webstack <nehaweb1993@gmail.com>
2020-06-10 22:06:22 +0530

    Using Images & CSS in Laravel

---
+++ resources/views/signup.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+@section('content')
+
+<div class="content">
+                <div class="title m-b-md">
+                  You can register here!
+                </div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+@extends('layout')
+@section('title')
+Home
+@endsection
+
+
+@section('content')
+<div class="content">
+<img src="/images/coverimg.png" alt="Cover Image"/>
+                <div class="title m-b-md">
+                    Laravel Demo Page
+                    
+                </div>
+                
+                <div class="links">
+                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://vapor.laravel.com">Vapor</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+
+            </div>
+           
+            VAT is {{ $tax }}
+@endsection
